81c00b03b54ef9ed0bdabcca4a01273bab548e78
aymaneagle <aymanalwahid04@gmail.com>
2025-11-24 22:56:14 +0600

    Fixed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import StayDetails from './pages/StayDetails';
 import MyBookings from './pages/MyBookings';
 import StayReg from './components/StayReg';
 import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
 
 
 const App = () => {
@@ -23,7 +25,11 @@ const isOwnerPath = useLocation().pathname.includes("owner")
           <Route path='/stays' element= {<AllStays/>} />
           <Route path='/stays/:id' element= {<StayDetails/>} />
           <Route path='/my-bookings' element= {<MyBookings/>} />
-          <Route path='/owner' element= {<Layout/>} />
+          <Route path='/owner' element= {<Layout/>} >
+            <Route index element={<Dashboard/>} />
+            <Route path="add-room" element={<AddRoom/>} />
+
+          </Route>    
         </Routes>
       </div>
         <Footer/>
