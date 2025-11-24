3c0916425e695a74be569b02c41a8f4f006368aa
aymaneagle <aymanalwahid04@gmail.com>
2025-11-24 23:47:14 +0600

    Fix Dashboard filename

---
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from "../../components/Title"
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Title font="outfit" align="left" title="Dashboard" subTitle="Monitor your stay listings, track bookings and analyze revenue-all in one place. Stay updated with real-time insights to ensure smooth operations." />
+        </div>
+    );
+};
+
+export default Dashboard;
